05a46b359071986ca46e61734514c9926af5f14c
Acethephoenix <acethephoenix2001@gmail.com>
2024-01-19 13:17:25 +0100

    réparation authentification

--- src/resources/admin/admin.route.js
+++ src/resources/admin/admin.route.js
@@ -1,6 +1,7 @@
-const {router} = require('express');
+const express = require('express');
 const {explosion} = require('./admin.controller');
 
+const router = express.Router();
 router.route('/admin').delete(explosion);
 
 module.exports = router;
--- src/resources/middlewares/authentification.js
+++ src/resources/middlewares/authentification.js
@@ -2,7 +2,12 @@ const jwt = require("jsonwebtoken");
 
 const authentification = (req, res, next) => {
   req.session.token = null
-  const token = req.headers.authorization.split(" ")[1];
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ ok: false, error: "No token provided" });
+  }
+  console.log(authorization);
+  const token = authorization.split(" ")[1];
   jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
     if (err) {
       return res.status(401).send({ ok: false, error: "Invalid token" });
